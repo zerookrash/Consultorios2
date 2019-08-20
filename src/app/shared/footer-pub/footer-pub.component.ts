e1d2201e112b17063ee7def485ef5cf933390d65
zerookrash <zerookrash@me.com>
2019-08-20 12:31:08 -0500

    Quitando Datos

--- src/app/shared/footer-pub/footer-pub.component.ts
+++ src/app/shared/footer-pub/footer-pub.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterPubComponent implements OnInit {
 
+  anio = new Date().getFullYear();
+
   constructor() { }
 
   ngOnInit() {
